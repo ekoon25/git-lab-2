15455e08170999d4c0cb1845f3bb0a7eca1079bd
Evan O'Koon <ek406220@ohio.edu>
2022-01-21 15:37:56 -0500

    Created C++ program

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Evan O'Koon
+ *        Date: 1/21/2022
+ * Description: Prints out a message
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    
+    return 0;
+}// main
